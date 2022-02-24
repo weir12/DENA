b888322e1106e8a05e35c449aad89c79e972507b
Liang Ou <37523816+weir12@users.noreply.github.com>
2022-02-24 11:09:08 +0800

    Update LSTM_predict.py

--- step4_predict/LSTM_predict.py
+++ step4_predict/LSTM_predict.py
@@ -14,6 +14,7 @@ import pickle,subprocess
 from collections import defaultdict
 from collections import Counter
 import numpy as np
+import pandas as pd
 import glob,os,pdb
 import difflib
 import argparse
